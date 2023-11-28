649f8e0a0ea98a29d859ffdac7acb1043847ab80
AgustinSolaberrieta <agustinasolaberrieta08@gmail.com>
2023-11-28 18:43:38 -0300

    primer commit

---
+++ por/my-react-tailwind-project/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-gray-200 p-4">
+        <h1 className="text-3xl font-bold ">Hello, Tailwind CSS!</h1>
+        <p className="mt-2 text-gray-700">
+          This is a React app with Tailwind CSS.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
